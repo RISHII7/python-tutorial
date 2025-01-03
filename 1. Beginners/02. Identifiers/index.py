21f6755243a51ee5821f5816d664ed2be79cb2af
RISHII7 <rishikesh.palande07@gmail.com>
2025-01-03 14:15:01 +0530

    Identifiers in Python

---
+++ 1. Beginners/02. Identifiers/index.py
@@ -0,0 +1,15 @@
+# Variables naming
+
+# - Case Sensitivity
+password = 12345
+# PASSWORD: 54321 ❌
+# password: 54321 ✅
+print(password)
+
+# Reserved Words ❌
+# while = 20
+# print(while)
+
+# Underscores
+user_password = 20
+print(user_password)
