87f3faa1dac39d4453e7afd5ff77406c58983bff
RISHII7 <rishikesh.palande07@gmail.com>
2025-01-03 14:25:53 +0530

    Numbers in Python

---
+++ 1. Beginners/03. Numbers/live coding/index.py
@@ -0,0 +1,37 @@
+# Addition
+sum_result = 5 + 7
+print(sum_result)
+
+# Subtraction
+sub_result = 10 - 3
+print(sub_result)
+
+# Multiplication
+mul_result = 4 * 9
+print(mul_result)
+
+# Division
+div_result = 12 / 4
+print(div_result)
+
+rounded_val = 12 // 4
+print(rounded_val)  # 3 👈 rounded
+
+# Exponentiation
+exp_result = 2**3
+print(exp_result)
+
+# Modulo
+mod_result = 17 % 5
+print(mod_result)  # 2 👈 remainder
+
+# Floating-point numbers
+float_num = 3.14
+
+# Rounding
+rounded_num = round(float_num, 2)
+print(rounded_num)  # 3.14
+
+# Conversion from int to float
+int_to_float = float(5)
+print(int_to_float)  # 5.0
