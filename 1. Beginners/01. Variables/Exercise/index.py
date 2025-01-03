a157b688962409da35b3523f1db35ec713465e4b
RISHII7 <rishikesh.palande07@gmail.com>
2025-01-03 13:35:24 +0530

    Variables in Python

---
+++ 1. Beginners/01. Variables/Exercise/index.py
@@ -0,0 +1,5 @@
+name = "Rishi"
+age = 23
+city = "Pune"
+
+print("My name is: ", name, "\nMy age is: ", age, "\nI live in: ", city)
---
+++ 1. Beginners/01. Variables/Live Coding/index.py
@@ -0,0 +1,7 @@
+# Creating Variables
+age = 10
+print(age)
+
+# Changing variables value
+age = 20
+print(age)
